96201880a0fe9c47e9d53a6c3c1524c3c4639e23
karbionletter <grizlexy@gmail.com>
2025-04-09 03:34:17 +0300

    Update wsgi.py

--- studier_django/studier_django/wsgi.py
+++ studier_django/studier_django/wsgi.py
@@ -11,8 +11,6 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, 'C:/Env/localassets')
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localassets.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studyer_django.settings')
 
 application = get_wsgi_application()
