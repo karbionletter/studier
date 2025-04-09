44210bd3916ddcae1c61c92f91cdce188387ec62
karbionletter <grizlexy@gmail.com>
2025-04-09 03:34:26 +0300

    Update wsgi.py

--- studier_django/studier_django/wsgi.py
+++ studier_django/studier_django/wsgi.py
@@ -11,6 +11,6 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studyer_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studier_django.settings')
 
 application = get_wsgi_application()
